e54be5f4ab204ff4c741151527c0bce5004ac102
aalok-29 <65300042+aalok-29@users.noreply.github.com>
2020-08-06 21:44:09 +0530

    Update switch.c

--- LPC2148/Switch Interface/switch.c
+++ LPC2148/Switch Interface/switch.c
@@ -8,10 +8,10 @@ int main(void)
 	
 	while(1)
 	{
-		IO0PIN=IO0PIN | 0x00000001;
-		if((IO0PIN && 0x00000002 )== 0x00000000)
+		IO0PIN=IO0PIN | 0x00000001; //sets the pin 0.00 as high
+		if((IO0PIN && 0x00000001 )== 0x00000000)//if pin 0.0 is low then set pin 0.2 as high 
 		{
-		IO0PIN=IO0PIN | 0x00000004;
+		IO0PIN=IO0PIN | 0x00000004;//setting pin 0.2 as high
 		}
 	
 	}
