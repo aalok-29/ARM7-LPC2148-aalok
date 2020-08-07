813c4f65030de410d6c90aa5004556082e287c3d
aalok-29 <65300042+aalok-29@users.noreply.github.com>
2020-08-07 15:51:33 +0530

    Update trafficSignal.c

--- LPC2148/Traffic Signal/trafficSignal.c
+++ LPC2148/Traffic Signal/trafficSignal.c
@@ -1,31 +1,29 @@
-#include<lpc214x.h> //importing LPC2148 library
+#include<lpc214x.h>
 #include<stdint.h>
-
-void delay_ms(uint16_t j) //function for delay
+void delay()// function for 500ms delay
 {
-	uint16_t x,i;
-	for(i=0;i<j;++i)
-	{
-		for(x=0;x<60000;x++);
-	}
+  T0MR0=1000000;    // load count for delay
+T0MCR= 0X00000004; //stop on MR0
+T0TCR=1;           //enable timer 0
+while(T0TC!=T0MR0)	; // wait until TC reaches desired delay
+	T0TC=0;           // reset timer
 	
 }
 
+
 int main(void)
-{
-	IO0DIR=0x000000FF;  // initializing pins 0.0 - 0.7 as output pins
+{int i;
+	
 	while(1)
 	{
-		IO0PIN=IO0PIN | 0x00000001; //sets pin 0.0 as high
-		delay_ms(300);              //delay of 300 milliseconds
-		IO0PIN=IO0PIN & 0xFFFFFF00;//sets pin 0.0 as low
 		
-		IO0PIN=IO0PIN | 0x00000002;//sets pin 0.1 as high
-		delay_ms(300);             //delay of 300 milliseconds
-		IO0PIN=IO0PIN & 0xFFFFFF00;//sets pin 0.1 aslow
-		
-		IO0PIN=IO0PIN | 0x00000004;//sets pin 0.2 as high
-		delay_ms(300);             //delay of 300 milliseconds
-		IO0PIN=IO0PIN & 0xFFFFFF00;//sets pin 0.2 as low
+		for(i=5;i<8;++i)
+		{
+		IO0DIR=(1<<i); //initializing pin 10
+		IO0SET = (1<<i);//setting pin 10 to high
+		delay();       //wait for 500ms
+		IO0CLR = (1<<i); //setting pin 10 to low
+		delay();        //wait for 500ms
+		}
 	}
 }
